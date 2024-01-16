db59980339569ef81006f682bf49c1873e73be9c
Camille <camille.bedeche@laposte.net>
2024-01-16 10:14:04 +0100

    Exercie 1

---
+++ exercice1/script.js
@@ -0,0 +1,12 @@
+let conteneur = document.getElementById("conteneur");
+console.log(conteneur);
+
+let paragraphes = document.getElementsByTagName("p");
+console.log(paragraphes);
+
+let test = document.getElementsByClassName("test")[0];
+
+console.log(test);
+
+let liens = document.querySelectorAll("a[href='#']");
+console.log(liens);
---
+++ je_sais_pas.js
@@ -0,0 +1,9 @@
+/**
+ * QuerySelector
+ */
+
+let elt = document.querySelector("paragraphe");
+console.log(elt);
+
+let elts = document.querySelectorAll("paragraphe");
+console.log(elts);
