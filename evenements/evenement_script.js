522d8862841a0198b71a0e7874a040e84c0d5bc5
Camille <camille.bedeche@laposte.net>
2024-01-16 17:01:24 +0100

    travil

--- evenements/evenement_script.js
+++ evenements/evenement_script.js
@@ -2,7 +2,17 @@
  * Event click
  */
 
-document.querySelector('#mon4Btn').addEventListener('click', function () {
+document.querySelector('#monBtn').addEventListener('click', function () {
     alert('test addEventListener');
 });
+
+function w3c() {
+    let optionReponse = document.querySelector("select");
+
+    if (optionReponse.value !== "1994") {
+        alert("Mauvais réponse la W3C à été créé par Tim en 1994");
+    } else {
+        alert("Bonne réponse ! Elle a été créé par Tim");
+    }
+}
 
